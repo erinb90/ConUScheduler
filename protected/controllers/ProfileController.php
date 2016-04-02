bcf9a5f81128779f3940a2e464255ff2b8774278
Bryce Drewery Schoeler <darktarro100@gmail.com>
2016-04-02 17:05:58 -0400

    issue #104 fixed divide by 0

--- protected/controllers/ProfileController.php
+++ protected/controllers/ProfileController.php
@@ -6,6 +6,7 @@
  * Date: 3/6/2016
  * Time: 11:44 AM
  */
+use app\models\CompletedCoursesForm;
 class ProfileController extends Controller
 {
 
@@ -32,5 +33,45 @@ class ProfileController extends Controller
         return array(
             'accessControl',
         );
+    }
+
+
+    public function actionAddCompleted()
+    {
+        $model = new PreferenceForm();
+        $model = new app\models\CompletedCoursesForm();
+        Yii::app()->db->createCommand()->insert('completed_courses', array(
+            'userId'=>19,
+            'courseId'=>5,
+            'Grade'=>"A+",
+            'GPA'=>4.3,
+        ));;
+
+        if ($model->load(Yii::$app->request->post())) {
+            // valid data received in $model
+            $temp=$_POST['courseCode'];
+            $data="\"$temp\"";
+            $sql="SELECT * FROM course WHERE course_code=$data";
+            $test=Yii::app()->db->createCommand($sql);
+            $data=$test->queryAll();
+            $id_request=$data[0]["ID"];
+            $grade_to_gpa=array("F"=>0,"D-"=>0.7,"D"=>1,"D+"=>1.3,"C-"=>1.7,"C"=>2.0,"C+"=>2.3,"B-"=>2.7,"B"=>3.0,"B+"=>3.3,"A-"=>3.7,"A"=>4.0,"A+"=>4.3,);
+            $id=Yii::app()->user->userID;
+            $gpa=$grade_to_gpa[$_POST['grade']];
+            $grade=$_POST['grade'];
+            Yii::app()->db->createCommand()->insert('completed_courses', array(
+                'userId'=>$id,
+                'courseId'=>$id_request,
+                'Grade'=>"$grade",
+                'GPA'=>$gpa,
+            ));;
+
+            // do something meaningful here about $model ...
+
+            return $this->render('record', ['model' => $model]);
+        } else {
+            // either the page is initially displayed or there is some validation error
+            return $this->render('record', ['model' => $model]);
+        }
     }
 }
--- protected/views/profile/record.php
+++ protected/views/profile/record.php
@@ -1,4 +1,10 @@
 <?php
+/* @var $this ProfileController */
+/* @var $model CompletedCourses */
+/* @var $form CActiveForm */
+?>
+
+<?php
 $this->menu=array(
     array('label'=>'Profile', 'url'=>array('index')),
     array('label'=>'Contact Information', 'url'=>array('admin')),
@@ -26,7 +32,7 @@ $test=Yii::app()->db->createCommand($sql);
 $data=$test->queryAll();
 $creditTotal=0;
 $sum=0;
-
+$finalGPA=null;
 foreach ($data as $value)
 {
     if ($value['userID']== Yii::app()->user->userID) {
@@ -36,10 +42,52 @@ foreach ($data as $value)
     }
 
 }
-$finalGPA=$sum/$creditTotal;
-$finalGPA=round($finalGPA,2);
+if($creditTotal>0)
+{
+    $finalGPA=$sum/$creditTotal;
+    $finalGPA=round($finalGPA,2);
+}
 echo "<h2>";
 
 echo "Total GPA: $finalGPA";
 echo "</h2>";
+$id=Yii::app()->user->userID;
+$data="\"COMP 346\"";
+$sql="SELECT * FROM course WHERE course_code=$data";
+$test=Yii::app()->db->createCommand($sql);
+$data=$test->queryAll();
+$id_request=$data[0]["ID"];
+
+$test=Yii::app()->db->createCommand($sql);
+$data=$test->queryAll();
+
 ?>
+
+
+
+
+    <div class="form">
+<?php $form=$this->beginWidget('CActiveForm'); ?>
+
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'courseID'); ?>
+        <?php echo $form->textField($model,'courseID'); ?>
+        <?php echo $form->error($model,'courseID'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model,'Grade'); ?>
+        <?php echo $form->textField($model,'Grade'); ?>
+        <?php echo $form->error($model,'Grade'); ?>
+
+    </div>
+
+
+    <div class="row buttons">
+        <?php echo CHtml::button('Submit', array('submit' =>  array('profile/AddCompleted'))); ?>
+    </div>
+
+<?php $this->endWidget(); ?>
+    </div>
